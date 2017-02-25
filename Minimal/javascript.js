ff7d82e417bea9d4ffb4fa5e10a3b4b2fba4754d
areking <prastor159@gmail.com>
2017-02-25 08:26:41 +0100

    ok

--- Minimal/javascript.js
+++ Minimal/javascript.js
@@ -67,10 +67,12 @@
 	]
 	
 		
+	//ok
 	var google = 'http://www.google.com/search?q=';
 	var duckduckgo = 'https://duckduckgo.com/?q=';
 	var defaultSearch = google;
 
+	
 	/* INPUT SEARCH */
 	
 	var input1 = document.getElementById('input1');
